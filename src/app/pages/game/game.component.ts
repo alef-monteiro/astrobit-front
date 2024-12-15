041617f726dd5f9117153cfe82eb4f267092ced4
álef monteiro <72264836+alef-monteiro@users.noreply.github.com>
2024-12-14 22:14:46 -0400

    feat: Implements functional iframe. (#32)

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,42 +1,69 @@
 import {
   Component, OnInit
 } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Card} from '../../../shared/models/card';
-import {GameCardDataService} from '../../../shared/services/game-card-data.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Card } from '../../../shared/models/card';
+import { GameCardDataService } from '../../../shared/services/game-card-data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [],
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss'] // Corrigido para 'styleUrls'
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
   public object: Card = new Card();
+  public safeUrl: SafeResourceUrl;
 
   constructor(
     private router: Router,
     public route: ActivatedRoute,
-    public service: GameCardDataService
-  ) {}
+    public service: GameCardDataService,
+    private sanitizer: DomSanitizer,
+  ) {
+  }
 
   public onNavigate() {
-    this.router.navigate(['game'])
+    this.router.navigate(['game']).then();
   }
 
   ngOnInit() {
-    if(this.route.snapshot.paramMap.get('action') === 'create'){
+    // Verifica se o link está disponível antes de sanitizar
+    const link = this.object.link;
+    if (link) {
+      this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(link);
+    } else {
+      console.warn('Link não está disponível para sanitização');
+    }
+
+    // Verifica se a ação é 'create' e loga no console
+    if (this.route.snapshot.paramMap.get('action') === 'create') {
       console.log('create');
-    }else {
-      this.object.id = Number(this.route.snapshot.paramMap.get('action'));
-      this.service.getGameCardById(this.object.id).subscribe(
-        (response) => {
-          console.log(response);
-          this.object = response;
-        }
-      )
+    } else {
+      // Obtém o ID da URL e faz a requisição para obter os dados do jogo
+      const gameId = Number(this.route.snapshot.paramMap.get('action'));
+      if (gameId) {
+        this.object.id = gameId;
+        this.service.getGameCardById(this.object.id).subscribe(
+          {
+            next: (response) => {
+              console.log(response);
+              this.object = response;
+
+              // Garantir que a URL seja sanitizada após receber os dados
+              if (this.object.link) {
+                this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.object.link);
+              }
+            },
+            error: (error) => {
+              console.error('Erro ao buscar os dados do jogo:', error);
+            }
+          });
+      } else {
+        console.warn('ID do jogo não é válido');
+      }
     }
   }
 }
